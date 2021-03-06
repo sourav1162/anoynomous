509ec9f2da00416007f17388ed043eb3903cdd9a
sourav1162 <63467856+sourav1162@users.noreply.github.com>
2021-03-06 11:48:06 +0530

    Add files via upload

---
+++ redbusIndia/redbusIndia/index.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace redbusIndia
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void search_btn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://localhost:50618/rpool.aspx");
+        }
+    }
+}
